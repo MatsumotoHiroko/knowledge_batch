d82eba9f1563754835fbe93e05134dd7475bb61b
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-06-30 11:00:11 +0900

    update

--- redmine_import.py
+++ redmine_import.py
@@ -7,6 +7,9 @@ db = dataset.connect('postgresql://postgres@localhost:5432/knowledge_import_test
 # 管理者を取得
 manager = db['users'].find_one(user_name='管理者ユーザ')
 print(manager['user_id'])
+# 現在の最終記事を取得
+last_knowledge = db['knowledge'].find_one(order_by='knowledge_id')
+print(last_knowledge)
 # ['#', 'ステータス', 'プロジェクト', 'トラッカー', '優先度', '題名', '担当者', 'カテゴリ', '対象バージョン', '作成者', '開始日', '期日', '進捗 %', '予定工数', '親チケット', '作成日', '更新日', 'PL', 'タグ', '説明']
 df = pd.read_csv('redmine_export.csv', header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'contents'))
 df = df.reindex_axis(['title', 'contents', 'insert_datetime', 'update_datetime'], axis=1)
@@ -23,5 +26,6 @@ df['delete_flag'] = 0
 
 dictionary = df.T.to_dict().values()
 
-db['knowledges'].insert_many(dictionary)
+#db['knowledges'].insert_many(dictionary)
 
+    
