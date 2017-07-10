cb7b1c1f420482bd85ee2695536159a8afabc684
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-07-10 11:21:32 +0900

    content name error

--- webapi_import_redmine.py
+++ webapi_import_redmine.py
@@ -41,8 +41,8 @@ def run_post_api(df):
 
 
 # ['#', 'ステータス', 'プロジェクト', 'トラッカー', '優先度', '題名', '担当者', 'カテゴリ', '対象バージョン', '作成者', '開始日', '期日', '進捗 %', '予定工数', '親チケット', '作成日', '更新日', 'PL', 'タグ', '説明']
-df = pd.read_csv('redmine_export.csv', header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'contents'))
-df = df.reindex_axis(['title', 'contents'], axis=1)
+df = pd.read_csv('redmine_export.csv', header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'content'))
+df = df.reindex_axis(['title', 'content'], axis=1)
 dictionary = df.T.to_dict().values()
 for d in dictionary:
     run_post_api(d)
