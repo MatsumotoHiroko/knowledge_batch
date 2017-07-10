4d86a21a7e5b19093a6fd65ed94ba08f168fc946
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-07-10 11:15:48 +0900

    edit

--- webapi_import_redmine.py
+++ webapi_import_redmine.py
@@ -2,7 +2,6 @@
 
 import json
 import requests
-import dataset
 import pandas as pd
 
 api_url = 'http://192.168.11.202:8080/knowledge'
@@ -40,16 +39,11 @@ def run_post_api(df):
     print(r.text)
     print(r.headers)
 
-db = dataset.connect(db_url)
-
-# 管理者を取得
-manager = db['users'].find_one(user_name='管理者ユーザ')
-# 現在の最終記事を取得
-last_knowledge = db['knowledges'].find_one(order_by='-knowledge_id')
 
 # ['#', 'ステータス', 'プロジェクト', 'トラッカー', '優先度', '題名', '担当者', 'カテゴリ', '対象バージョン', '作成者', '開始日', '期日', '進捗 %', '予定工数', '親チケット', '作成日', '更新日', 'PL', 'タグ', '説明']
 df = pd.read_csv('redmine_export.csv', header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'contents'))
 df = df.reindex_axis(['title', 'contents'], axis=1)
+print(df)
 
 for d in df:
     run_post_api(d)
