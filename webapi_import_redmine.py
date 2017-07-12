366282d43b1c470c1d702d7caeb6ef00ab9b0e90
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-07-12 16:41:42 +0900

    dirname

--- webapi_import_redmine.py
+++ webapi_import_redmine.py
@@ -3,6 +3,8 @@
 import json
 import requests
 import pandas as pd
+import os
+import os.path
 import glob
 
 api_url = 'http://192.168.11.202:8080/knowledge/api/knowledges'
@@ -50,13 +52,16 @@ def run_post_api(d):
     print(r.text)
     print(r.headers)
 
-
-for filename in glob.iglob(csv_dir + '/*.csv'):
-    # ['#', 'ステータス', 'プロジェクト', 'トラッカー', '優先度', '題名', '担当者', 'カテゴリ', '対象バージョン', '作成者', '開始日', '期日', '進捗 %', '予定工数', '親チケット', '作成日', '更新日', 'PL', 'タグ', '説明']
-    df = pd.read_csv(filename, header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'content'))
-    df = df.reindex_axis(['title', 'content', 'staff'], axis=1)
-    dictionary = df.T.to_dict().values()
-    print(dictionary)
-    for d in dictionary:
-        #run_post_api(d)
-        break
+for dirname in glob.iglob(csv_dir + '/*'):
+    if os.path.isdir(dirname)
+        print(dirname)
+        for filename in glob.iglob(dirname + '/*.csv'):
+            print(filename)
+            # ['#', 'ステータス', 'プロジェクト', 'トラッカー', '優先度', '題名', '担当者', 'カテゴリ', '対象バージョン', '作成者', '開始日', '期日', '進捗 %', '予定工数', '親チケット', '作成日', '更新日', 'PL', 'タグ', '説明']
+            df = pd.read_csv(filename, header=1, names=('index', 'status', 'project', 'tracker', 'priority', 'title', 'staff', 'category', 'version', 'creator', 'started_at', 'limited_at', 'progress_rate', 'workload', 'parent_ticket', 'insert_datetime', 'update_datetime', 'pl', 'tag', 'content'))
+            df = df.reindex_axis(['title', 'content', 'staff'], axis=1)
+            dictionary = df.T.to_dict().values()
+            print(dictionary)
+            for d in dictionary:
+                #run_post_api(d)
+                break
