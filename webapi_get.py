8a0b4cd63d2aa45b350bf2900847f319dcf404c5
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-07-12 11:47:49 +0900

    get particuler knowledge

---
+++ webapi_get.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+knowledge_id = '43'
+url = 'http://192.168.11.202:8080/knowledge/api/knowledges' + knowledge_id
+token = 'Access token'
+
+query = {
+          'private_token': token,
+        }
+
+r = requests.get(
+        url,
+        params=query)
+print(r)
+print(r.text)
+print(r.headers)
